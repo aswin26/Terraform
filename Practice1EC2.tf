556408aa8f6420621e42f2ff235b3f80f114f4b7
aswin26 <41717855+aswin26@users.noreply.github.com>
2022-06-14 21:36:51 +0530

    Add files via upload
    EC2 instance creation

---
+++ Practice1EC2.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+	access_key = "A"
+  secret_key = "CL"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "firstExample"{
+  ami = "ami-0fa49cc9dc8d62c84"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "First-server"
+    Environment = "dev"
+  }
+}
